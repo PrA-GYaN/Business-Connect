0d6c4ac5be45935df10e995684134e058a2efb3a
PrA-GYaN <pragyan1516@gmail.com>
2024-11-25 09:03:06 +0545

    Message

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: false,
+  },
   server: {
     host: '0.0.0.0', // Allow external connections
     port: 5173,       // Make sure this is the port you want
