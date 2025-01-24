9a48aadb964d85aea810b54c22169bd059486ff5
PrA-GYaN <pragyan1516@gmail.com>
2025-01-24 13:19:11 +0545

    Config Changes

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,8 +7,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     cssMinify: false,
+    minify: false,
   },
   server: {
     port: process.env.PORT || 5173,
+    host: '0.0.0.0',
   },
 })
